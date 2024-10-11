11b9588e6474ce2cc96a350c02831feb566f6e36
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-10-12 07:56:14 +0900

    테스트 코드는 하나의 주제만 포함한다

--- src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
@@ -33,4 +33,27 @@ class ProductTypeTest {
         assertThat(result).isTrue();
     }
 
+    @Test
+    @DisplayName("상품 타입이 재고 관련 타입인지 체크한다.")
+    void containsStockTypeEx() { // 상대방이 생각하게끔 만드는 테스트 코드는 지양한다.
+        //given
+        ProductType[] productTypes = ProductType.values();
+
+        for(ProductType type: productTypes) {
+            if(type == ProductType.HANDMADE) {
+                // when
+                boolean result = ProductType.containsStockType(type);
+                // then
+                assertThat(result).isFalse();
+            }
+
+            if(type == ProductType.BOTTLE || type == ProductType.BAKERY) {
+                // when
+                boolean result = ProductType.containsStockType(type);
+                // then
+                assertThat(result).isTrue();
+            }
+        }
+    }
+
 }
