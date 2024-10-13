87a5cf2e82cf791d55601235e98ba71333f6b718
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-10-13 21:06:54 +0900

    Repository와 Service를 같은 테스트 환경으로 설정

--- src/test/java/sample/cafekiosk/spring/domain/ProductRepositoryTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/ProductRepositoryTest.java
@@ -11,16 +11,15 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+import sample.cafekiosk.spring.IntegrationTestSupport;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 import sample.cafekiosk.spring.domain.product.ProductType;
 
-@DataJpaTest
-@ActiveProfiles("test")
-class ProductRepositoryTest {
+@Transactional
+class ProductRepositoryTest extends IntegrationTestSupport {
 
     @Autowired
     private ProductRepository productRepository;
